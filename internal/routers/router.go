c83caf4a847491fcc44e2df4e4cac4b7c645eec6
liumingxin <minngxin@qq.com>
2022-03-02 13:34:04 +0800

    接口增加参数校验

--- internal/routers/router.go
+++ internal/routers/router.go
@@ -3,6 +3,7 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/oddminng/go-blog-service/docs"
+	"github.com/oddminng/go-blog-service/internal/middleware"
 	v1 "github.com/oddminng/go-blog-service/internal/routers/v1"
 	ginSwagger "github.com/swaggo/gin-swagger"
 	"github.com/swaggo/gin-swagger/swaggerFiles"
@@ -12,6 +13,7 @@ func NewRouter() *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
+	r.Use(middleware.Translations())
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	article := v1.NewArticle()
